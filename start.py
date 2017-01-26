8ed7a6c41535feb1a60d6f716dd928e64d05b185
darkchylde <georgejpadayatty0000@gmail.com>
2017-01-26 14:42:47 +0530

    Speeding up brain load

--- start.py
+++ start.py
@@ -1,11 +1,18 @@
 import aiml
+import os
 
 # Create the kernel and learn AIML files
 
 kernel = aiml.Kernel()
-kernel.learn("std-startup.xml")
-kernel.respond("load aiml b")
 
+
+if os.path.isfile("bot_brain.brn"):
+	kernel.bootstrap(brainFile = "bot_brain.brn")
+else:
+	kernel.bootstrap(learnFiles = "std-startup.xml",commands = "load aiml b")
+	kernel.saveBrain("bot_brain.brn")
+
+# kernel is now ready for use.
 # Press CTRL-C to break this loop
 while True:
 	print kernel.respond(raw_input("Enter your message >> "))
